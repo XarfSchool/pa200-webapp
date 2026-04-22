e79a344f63e55cb71bd8a7f8e130c03ee4dce67c
Tomas Bozek <Tomas.Bozek@seacomp.cz>
2026-04-23 00:21:37 +0200

    preparing project for azure blob storage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+};
+
+module.exports = {
+  // Use the Storage URL in production, otherwise use relative paths
+  assetPrefix: isProd ? process.env.BLOB_URL : undefined,
 };
 
 export default nextConfig;
